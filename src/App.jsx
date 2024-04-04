b330226b9930098f3609f73e77ab11c1329865c2
Consoling <shahidarizfannymagnet@gmail.com>
2024-04-05 01:47:31 +0530

    created routes for multiple pages and modified navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import Navbar from './components/Navbar/navbar.jsx'
-import Banner from './components/Banner/banner.jsx'
-import Hero from './components/Hero/hero.jsx'
+import React from "react";
 
+import "./App.css";
+
+import {  Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Contact from "../pages/Contact";
+import About from "../pages/About";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Product from "../pages/Product";
+import Products from "../pages/Products";
 
 const App = () => {
   return (
-    <div><Navbar/> <Banner/> <Hero/></div>
-  )
-}
+    
+      <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/contact-us" element={<Contact />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/product" element={<Product />}></Route>
+      <Route path="/products" element={<Products />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/signup" element={<Signup />}></Route>
+     </Routes> 
+    
+  );
+};
 
-export default App
+export default App;
--- src/components/Banner/banner.jsx
+++ src/components/Banner/banner.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./banner.css";
+import { Zoom } from "@mui/material";
+import {Slide} from '@mui/material'
 const Banner = () => {
   return (
-    <div className="container">
-      <div className="elements_container">
-        <div className="t-shirt"> <h1>Your T-Shirt</h1></div>
-        <div className="banner"><img src="banner.jpg" alt="" /></div>
-      </div>
+    <div  className="container">
+<div className="inner-container">
+  <div className="banner-container"><img src='banner.jpg' alt="banner" /></div>
+  <div className="t-shirt_container">
+    <img src="/t-shirt1.png" alt="t-shirt" />
+    <h1>Your T-Shirt</h1>
+  </div>
+</div>
     </div>
   );
 };
