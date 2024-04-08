c5f85ca37fed9cbf43949566b7378e38d10f3324
Consoling <shahidarizfannymagnet@gmail.com>
2024-04-08 12:06:00 +0530

    SIdebar fixed

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -10,7 +10,7 @@ const Sidebar = ({ children }) => {
   return (
     <aside className="h-screen">
       <nav className="h-full flex flex-col bg-white border-r shadow-sm">
-        <div className="p-4 pb-2 flex justify-between items-center mb-8">
+        <div className="p-4 pb-2 flex justify-between items-center mb-1">
           {/* <img
             src="https://img.logoipsum.com/243.svg"
             className={`overflow-hidden transition-all ${
@@ -18,36 +18,37 @@ const Sidebar = ({ children }) => {
             }`}
             alt=""
           /> */}
-          <h2 className="font-bold text-xl ">Admin Panel</h2>
+          <h2 className={`${expanded ? 'font-bold text-xl' : 'hidden' } `}>Admin Panel</h2>
           <button
             onClick={() => setExpanded((curr) => !curr)}
-            className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100"
+            className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100 w-0 m-3"
           >
             {expanded ? <ChevronFirst /> : <ChevronLast />}
           </button>
         </div>
+        <hr />
 
         <SidebarContext.Provider value={{ expanded }}>
-          <ul className="flex-1 px-3 ">{children}</ul>
+          <ul className="flex-1 px-3 mt-8">{children}</ul>
         </SidebarContext.Provider>
 
         <div className="border-t flex p-3">
           <img
             src={faker.image.avatar()}
             alt=""
-            className="w-10 h-10 rounded-md"
+            className="w-10 h-10 rounded-md hover:cursor-pointer"
           />
           <div
             className={`
               flex justify-between items-center
-              overflow-hidden transition-all ${expanded ? "w-52 ml-3" : "w-0"}
+              overflow-hidden transition-all hover:cursor-pointer ${expanded ? "w-52 ml-3" : "w-0"}
           `}
           >
-            <div className="leading-4">
+            <div className="leading-4 ">
               <h4 className="font-semibold">{faker.person.fullName()}</h4>
               <span className="text-xs text-gray-600">{faker.person.fullName()}@gmail.com</span>
             </div>
-            <MoreVertical size={20} />
+            <MoreVertical size={20} className="cursor-pointer hover:bg-gray-300 hover:rounded-xl "/>
           </div>
         </div>
       </nav>
