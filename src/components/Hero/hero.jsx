6b349c920d8afda09e9bfdb499eaaa3bb4409c78
Priyanka-12ggjk <nitupriyanka1234@gmail.com>
2024-04-11 18:10:08 +0530

    completed the Hero Section

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -2,8 +2,34 @@ import React from 'react'
 import './hero.css'
 const Hero = () => {
   return (
-    <div>
-      Hero
+    <div className="card-container">
+        <div className="card">
+        <img src="image1.jpg" />
+            <div className="card-content">
+                <h3>Card 1</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Sapiente, dolore! Possimus id voluptatibus eius earum eos</p>
+                <a href="" className="btn">Read More</a>
+            </div>
+        </div>
+        <div className="card">
+        <img src="image2.jpg" />
+            <div className="card-content">
+                <h3>Card 2</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Sapiente, dolore! Possimus id voluptatibus eius earum eos</p>
+                <a href="" className="btn">Read More</a>
+            </div>
+        </div>
+        <div className="card">
+            <img src="image3.jpg" />
+            <div className="card-content">
+                <h3>Card 3</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Sapiente, dolore! Possimus id voluptatibus eius earum eos</p>
+                <a href="" className="btn">Read More</a>
+            </div>
+        </div>
     </div>
   )
 }
