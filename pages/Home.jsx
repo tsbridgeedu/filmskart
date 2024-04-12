aba92d4797861de459d11cee85000cb08b6ce444
Consoling <shahidarizfannymagnet@gmail.com>
2024-04-12 21:17:07 +0530

    syncing Best Selling and trending section

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Banner from "../src/components/Banner/banner.jsx";
 import Hero from "../src/components/Hero/hero.jsx";
+import BestProducts from '../src/components/BestProducts/BestProducts.jsx'
 import Navbar from "../src/components/Navbar/navbar.jsx";
 import Footer from "../src/components/Footer/footer.jsx";
 
@@ -10,6 +11,7 @@ const Home = () => {
       <Navbar />
 
       <Banner />
+      <BestProducts />
       <Hero />
       <Footer />
     </div>
