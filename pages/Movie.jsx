464582d3da1d25e27c971bcd3f1d71a6f50e43bd
Consoling <shahidarizfannymagnet@gmail.com>
2024-04-15 12:20:37 +0530

    initiated movie description part

--- pages/Movie.jsx
+++ pages/Movie.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../src/components/Navbar/navbar'
 import MovieBanner from '../src/components/MovieBanner/movieBanner'
 import Footer from '../src/components/Footer/footer'
+import MovieDesc from '../src/components/MovieDesc/MovieDesc'
 
 const Movie = () => {
   return (
     <div>
         <Navbar/>
         <MovieBanner/>
+        <MovieDesc />
         <Footer/>
     </div>
   )
