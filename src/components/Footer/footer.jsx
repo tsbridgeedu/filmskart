16ec6d69d9fc7489da27c48294963c5084d780f1
RishikaDhiman <rishikadhiman2003@gmail.com>
2024-04-21 17:07:38 +0530

    commit

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -5,93 +5,143 @@ import './footer.css'
 
 const Footer = () => {
   return (
+    // <div className="footer">
+    //   <div className="sb__footer section_padding">
+    //     <div className="sb__footer-links">
+    //       <div className="sb__footer-links-div">
+    //         <h4>For Business</h4>
+    //         <hr/>
+    //         <NavLink to={"/"}>
+    //           <p>Employer</p>
+    //         </NavLink>
+    //         <NavLink to={"/"}>
+    //           <p>Get in Touch</p>
+    //         </NavLink>
+    //         <NavLink to={"/"}>
+    //           <p>Individuals</p>
+    //         </NavLink>
+    //       </div>
+
+    //       <div className="sb__footer-links-div">
+    //         <h4>Account</h4>
+    //         <hr/>
+    //         <NavLink to={"/"}>
+    //           <p>My Account</p>
+    //         </NavLink>
+    //         <NavLink to={"/"}>
+    //           <p>Login / Register</p>
+    //         </NavLink>
+    //         <NavLink to={"/"}>
+    //           <p>Cart</p>
+    //         </NavLink>
+    //         <NavLink to={"/"}>
+    //           <p>Wishlist</p>
+    //         </NavLink>
+    //         <NavLink to={"/"}>
+    //           <p>Shop</p>
+    //         </NavLink>
+    //       </div>
+
+    //       <div className="sb__footer-links-div">
+    //         <h4>Address</h4> 
+    //         <hr/>
+    //         <p style={{lineHeight:"1.5"}}>VijayNagar, Downtown Area, Los Santos, Hyderabad - 511001</p>
+    //       </div>
+
+    //       <div className="sb__footer-links-div">
+    //         <h4>Follow us on </h4>
+    //         <hr/>
+    //         <div className="socialmedia">   
+    //           <p>
+    //             <Instagram /> 
+    //           </p>
+    //           <p>
+    //             <Facebook />
+    //           </p>
+    //           <p>
+    //             <Twitter />
+    //           </p>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+
+    //   <hr />
+    //   <div className="sb__footer-below">
+    //     <div className="sb__footer-copyright">
+    //       <p className="font-bold tracking-widest text-base">@{new Date().getFullYear()} <a href="https://www.tsbridgeedu.in" className="no-underline ">TS Bridge Edu. All rights reserved</a></p>
+    //     </div>
+    //     <div className="sb__footer-below-links">
+    //       <NavLink to="/terms-and-conditions">
+    //         <div>
+    //           <p>Terms & Conditions</p>
+    //         </div>
+    //       </NavLink>
+    //       <NavLink to="/privacy-policy">
+    //         <div>
+    //           <p>Privacy Policy</p>
+    //         </div>
+    //       </NavLink>
+    //       <NavLink to="/faq">
+    //         <div>
+    //           <p>FAQ's</p>
+    //         </div>
+    //       </NavLink>
+    //       <NavLink to="/contact">
+    //         <div>
+    //           <p>Contact Us</p>
+    //         </div>
+    //       </NavLink>
+    //     </div>
+    //   </div>
+    // </div>
+
     <div className="footer">
-      <div className="sb__footer section_padding">
-        <div className="sb__footer-links">
-          <div className="sb__footer-links-div">
-            <h4>For Business</h4>
-            <hr/>
-            <NavLink to={"/"}>
-              <p>Employer</p>
-            </NavLink>
-            <NavLink to={"/"}>
-              <p>Get in Touch</p>
-            </NavLink>
-            <NavLink to={"/"}>
-              <p>Individuals</p>
-            </NavLink>
-          </div>
-
-          <div className="sb__footer-links-div">
-            <h4>Account</h4>
-            <hr/>
-            <NavLink to={"/"}>
-              <p>My Account</p>
-            </NavLink>
-            <NavLink to={"/"}>
-              <p>Login / Register</p>
-            </NavLink>
-            <NavLink to={"/"}>
-              <p>Cart</p>
-            </NavLink>
-            <NavLink to={"/"}>
-              <p>Wishlist</p>
-            </NavLink>
-            <NavLink to={"/"}>
-              <p>Shop</p>
-            </NavLink>
-          </div>
-
-          <div className="sb__footer-links-div">
-            <h4>Address</h4> 
-            <hr/>
-            <p style={{lineHeight:"1.5"}}>VijayNagar, Downtown Area, Los Santos, Hyderabad - 511001</p>
-          </div>
-
-          <div className="sb__footer-links-div">
-            <h4>Follow us on </h4>
-            <hr/>
-            <div className="socialmedia">   
-              <p>
-                <Instagram /> 
-              </p>
-              <p>
-                <Facebook />
-              </p>
-              <p>
-                <Twitter />
-              </p>
-            </div>
+      <div className="footer_data_box">
+        <div>
+          <h1>Celebrity Store</h1>
+          <div>
+            <Navlink><h2>Subscribe</h2></Navlink>
+            <Navlink><p>Get 10% off on your first order</p></Navlink>
+            <input placeholder="Enter your email"></input>
           </div>
         </div>
-      </div>
-
-      <hr />
-      <div className="sb__footer-below">
-        <div className="sb__footer-copyright">
-          <p className="font-bold tracking-widest text-base">@{new Date().getFullYear()} <a href="https://www.tsbridgeedu.in" className="no-underline ">TS Bridge Edu. All rights reserved</a></p>
+        <div>
+          <h1>Support</h1>
+          <div>
+            <Navlink><p>My Account</p></Navlink>
+            <Navlink><p>Login/Register</p></Navlink>
+            <Navlink><p>Cart</p></Navlink>
+            <Navlink><p>Wishlist</p></Navlink>
+            <Navlink><p>Shop</p></Navlink>
+          </div>
         </div>
-        <div className="sb__footer-below-links">
-          <NavLink to="/terms-and-conditions">
-            <div>
-              <p>Terms & Conditions</p>
-            </div>
-          </NavLink>
-          <NavLink to="/privacy-policy">
-            <div>
-              <p>Privacy Policy</p>
-            </div>
-          </NavLink>
-          <NavLink to="/faq">
-            <div>
-              <p>FAQ's</p>
-            </div>
-          </NavLink>
-          <NavLink to="/contact">
-            <div>
-              <p>Contact Us</p>
-            </div>
-          </NavLink>
+        <div>
+          <h1>Account</h1>
+          <div>
+            <Navlink><p>My Account</p></Navlink>
+            <Navlink><p>Login/Register</p></Navlink>
+            <Navlink><p>Cart</p></Navlink>
+            <Navlink><p>Wishlist</p></Navlink>
+            <Navlink><p>Shop</p></Navlink>
+          </div>
+        </div>
+        <div>
+          <h1>Quick Link</h1>
+          <div>
+            <Navlink><p>Privacy Policy</p></Navlink>
+            <Navlink><p>Terms Of Use</p></Navlink>
+            <Navlink><p>FAQ</p></Navlink>
+            <Navlink><p>Contact</p></Navlink>
+          </div>
+        </div>
+        <div>
+          <h1>Download App</h1>
+          <div>
+            <Navlink><h2>Subscribe</h2></Navlink>
+            <Navlink><p>Get 10% off on your first order</p></Navlink>
+            <input placeholder="Enter your email"></input>
+          </div>
         </div>
       </div>
     </div>
