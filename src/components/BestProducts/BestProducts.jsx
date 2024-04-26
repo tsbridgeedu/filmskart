e1f80c089a8f8b08c2e4c848255b5b80e01b9f20
Consoling <shahidarizfannymagnet@gmail.com>
2024-04-26 12:38:13 +0530

    overflow fix

--- src/components/BestProducts/BestProducts.jsx
+++ src/components/BestProducts/BestProducts.jsx
@@ -9,23 +9,23 @@ const BestProducts = () => {
       <div className="flex justify-between items-center w-full lg:px-10 relative px-4">
         <div className="start flex justify-center items-center">
           <div className="bg-red-500 w-2 h-10 rounded-md"> </div>
-          <div className="text-red-500 font-semibold text-2xl ml-3 tracking-wider">
+          <div className="text-red-500 font-semibold lg:text-2xl ml-3 tracking-wider">
             This Month
           </div>
         </div>
 
-        <div className="flex">
-          <p className="text-black text-6xl font-bold tracking-wide m-head">
+        {/* <div className="flex">
+          <p className="text-black lg:text-6xl font-bold tracking-wide m-head">
             Best Selling
           </p>
         </div>
 
         <div className="start flex justify-center items-center">
-          <div className="text-red-500 font-semibold text-2xl mr-3 tracking-wider">
+          <div className="text-red-500 font-semibold lg:text-2xl mr-3 tracking-wider">
             Celebrity Store
           </div>
           <div className="bg-red-500 w-2 h-10 rounded-md"> </div>
-        </div>
+        </div> */}
       </div>
 
       <div className="product-breaker flex ml-36 mr-36 mt-5">
