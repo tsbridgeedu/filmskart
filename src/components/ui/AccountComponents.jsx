c95b4591992a1fec3691106ab8afd4aab3b6e8de
Consoling <shahidarizfannymagnet@gmail.com>
2024-04-27 20:05:23 +0530

    changed Favicon and navbar

--- src/components/ui/AccountComponents.jsx
+++ src/components/ui/AccountComponents.jsx
@@ -1,67 +1,84 @@
 import MenuItem from "@mui/material/MenuItem";
-import Divider  from "@mui/material/Divider";
+import Divider from "@mui/material/Divider";
 import { SignOutButton, useUser } from "@clerk/clerk-react";
 
-import { AccountCircleOutlined,  SettingsApplicationsOutlined } from "@mui/icons-material";
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import {
+  AccountCircleOutlined,
+  SettingsApplicationsOutlined,
+} from "@mui/icons-material";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 
 import LoginIcon from "@mui/icons-material/Login";
 import HowToRegIcon from "@mui/icons-material/HowToReg";
 import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
 
-
 import { NavLink } from "react-router-dom";
 import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import { BsBagX, BsCart2 } from "react-icons/bs";
+import { ShoppingCart } from "lucide-react";
 
-export const SignedAccountComponents = ({handleClose}) => {
+export const SignedAccountComponents = ({ handleClose }) => {
+  const { user } = useUser();
 
-    const {user} = useUser()
-
-    return (
-        <>
-        <MenuItem sx={{my: 0.5}} onClick={handleClose}>
-          <Stack className="flex items-center justify-center w-full">
-          <AccountCircleOutlined sx={{ml: 1 }}/>
-          Hey {user.firstName}!
-          </Stack>
-          </MenuItem>
-          <Divider sx={{ my: 0.5 }} />
-          <MenuItem  disableRipple onClick={handleClose}>
-            <NavLink to={'/profile'}>
-              <SettingsApplicationsOutlined/>
-              Your Account
-            </NavLink>
-          </MenuItem>
-
-          <MenuItem>
-        <ExitToAppIcon />
-          <SignOutButton />
-          </MenuItem>
-        </>
-    )
-}
-
-export const SignedOutAccountComponents = ({handleClose}) => {
-  return(
+  return (
     <>
-    
-    <MenuItem onClick={handleClose} disableRipple>
-              <NavLink to={"/login"}>
-                <LoginIcon />
-                Login
-              </NavLink>
-            </MenuItem>
-            <MenuItem onClick={handleClose} disableRipple>
-              <NavLink to={"/signup"}>
-                <HowToRegIcon />
-                Sign Up
-              </NavLink>
-            </MenuItem>
-            <Divider sx={{ my: 0.5 }} />
-            <MenuItem onClick={handleClose} disableRipple>
-              <ManageAccountsIcon />
-              Profile
-            </MenuItem>
+      <MenuItem sx={{ my: 0.5 }} onClick={handleClose}>
+        <Stack className="flex items-center justify-center w-full">
+          <AccountCircleOutlined sx={{ ml: 1 }} />
+          Hey {user.firstName}!
+        </Stack>
+      </MenuItem>
+      <Divider sx={{ my: 0.5 }} />
+      <MenuItem disableRipple onClick={handleClose}>
+        <NavLink to={"/profile"}>
+          <SettingsApplicationsOutlined />
+          Your Account
+        </NavLink>
+      </MenuItem>
+
+      <MenuItem>
+        <ExitToAppIcon />
+        <SignOutButton />
+      </MenuItem>
     </>
-  )
-}
+  );
+};
+
+export const SignedOutAccountComponents = ({ handleClose }) => {
+  return (
+    <>
+     <MenuItem onClick={handleClose} disableRipple className="">
+        {/* <ManageAccountsIcon />
+              Profile */}
+
+        <Stack
+          className=" w-full"
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            maxWidth: "100%",
+            marginTop: '2px'
+          }}
+        >
+          <img src="/logo.png" alt="Filmskart Logo" className="w-28" />
+        </Stack>
+      </MenuItem>
+      <Divider sx={{ my: 0.5 }} />
+      <MenuItem onClick={handleClose} disableRipple>
+        <NavLink to={"/login"}>
+          <LoginIcon />
+          Login
+        </NavLink>
+      </MenuItem>
+      <MenuItem onClick={handleClose} disableRipple>
+        <NavLink to={"/signup"}>
+          <HowToRegIcon />
+          Sign Up
+        </NavLink>
+      </MenuItem>
+     
+    </>
+  );
+};
