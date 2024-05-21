9355a45f806bb45738d8b0214a13592298d8fdbf
singh-tanyaa <96328928+singh-tanyaa@users.noreply.github.com>
2024-05-21 13:55:26 +0530

    eventform issue

--- src/components/OrganiseForm.jsx
+++ src/components/OrganiseForm.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import * as Yup from "yup";
+import "../App.css";
+import "../index.css";
 
 const FormWithYup = () => {
   const [formData, setFormData] = useState({
@@ -63,51 +65,189 @@ const FormWithYup = () => {
   };
 
   return (
-    <div className="container">
-      <div className="modal">
-        <div className="modal-container">
-          <div className="modal-left p-10 pt-8 pb-6 bg-white flex-1 transition duration-500 opacity-100">
-            <h1 className="modal-desc text-purple-800 mt-1 mb-8">
-              Organise an Event!🤘
-            </h1>
-            <form className="form" onSubmit={handleSubmit}>
-              <div className="input-block flex flex-col p-2 border border-gray-300 rounded mb-2 transition duration-300">
-                <label className="input-label text-gray-600">First Name:</label>
-                <input
-                  type="text"
-                  name="firstName"
-                  value={formData.firstName}
-                  placeholder="Enter your first name"
-                  onChange={handleChange}
-                  className="outline-none border-0 py-1 text-sm placeholder-gray-400"
-                />
-                {errors.firstName && (
-                  <div className="error text-red-600">{errors.firstName}</div>
-                )}
+    <div className=" h-full font-nunito bg-[#EFEDEE] my-32">
+      <div className="  flex justify-center items-center bg-[#EFEDEE]">
+        <div className="w-full bg-[rgba(51,51,51,0.5)] flex flex-col items-center justify-center transition duration-400">
+          <div className="flex max-w-[60vw] w-full rounded-[10px] overflow-hidden absolute transition duration-300 bg-white">
+            <div className=" p-10 bg-white transition duration-500 opacity-100">
+              <h1 className="text-4xl text-[#86336e] mb-8">
+                Organise an Event!🤘
+              </h1>
+              <form className="flex flex-col gap-1" onSubmit={handleSubmit}>
+                <div className="flex flex-col p-2 border border-[#ddd] rounded-[4px] mb-2 transition duration-300 focus-within:border-[#8c7569]">
+                  <label className="text-xs uppercase font-semibold tracking-wide text-[#8c7569] transition duration-300">
+                    First Name:
+                  </label>
+                  <input
+                    type="text"
+                    name="firstName"
+                    value={formData.firstName}
+                    placeholder="Enter your first name"
+                    onChange={handleChange}
+                    className="outline-none border-0 pt-1 text-xs placeholder-gray-300"
+                  />
+                  {errors.firstName && (
+                    <div className="text-red-500">{errors.firstName}</div>
+                  )}
+                </div>
+                <div className="flex flex-col p-2 border border-[#ddd] rounded-[4px] mb-2 transition duration-300 focus-within:border-[#8c7569]">
+                  <label className="text-xs uppercase font-semibold tracking-wide text-[#8c7569] transition duration-300">
+                    Last Name:
+                  </label>
+                  <input
+                    type="text"
+                    name="lastName"
+                    value={formData.lastName}
+                    placeholder="Enter your last name"
+                    onChange={handleChange}
+                    className="outline-none border-0 pt-1 text-xs placeholder-gray-300"
+                  />
+                  {errors.lastName && (
+                    <div className="text-red-500">{errors.lastName}</div>
+                  )}
+                </div>
+                <div className="flex flex-col p-2 border border-[#ddd] rounded-[4px] mb-2 transition duration-300 focus-within:border-[#8c7569]">
+                  <label className="text-xs uppercase font-semibold tracking-wide text-[#8c7569] transition duration-300">
+                    Email:
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    placeholder="Enter your email"
+                    onChange={handleChange}
+                    className="outline-none border-0 pt-1 text-xs placeholder-gray-300"
+                  />
+                  {errors.email && (
+                    <div className="text-red-500">{errors.email}</div>
+                  )}
+                </div>
+                <div className="flex flex-col p-2 border border-[#ddd] rounded-[4px] mb-2 transition duration-300 focus-within:border-[#8c7569]">
+                  <label className="text-xs uppercase font-semibold tracking-wide text-[#8c7569] transition duration-300">
+                    Contact Number:
+                  </label>
+                  <input
+                    type="text"
+                    name="contactNumber"
+                    value={formData.contactNumber}
+                    placeholder="Enter your phone number"
+                    onChange={handleChange}
+                    className="outline-none border-0 pt-1 text-xs placeholder-gray-300"
+                  />
+                  {errors.contactNumber && (
+                    <div className="text-red-500">{errors.contactNumber}</div>
+                  )}
+                </div>
+                <div className="flex flex-col p-2 border border-[#ddd] rounded-[4px] mb-2 transition duration-300 focus-within:border-[#8c7569]">
+                  <label className="text-xs uppercase font-semibold tracking-wide text-[#8c7569] transition duration-300">
+                    Location:
+                  </label>
+                  <input
+                    type="text"
+                    name="location"
+                    value={formData.location}
+                    placeholder="Enter your location"
+                    onChange={handleChange}
+                    className="outline-none border-0 pt-1 text-xs placeholder-gray-300"
+                  />
+                  {errors.location && (
+                    <div className="text-red-500">{errors.location}</div>
+                  )}
+                </div>
+                <div className="flex flex-col p-2 border border-[#ddd] rounded-[4px] mb-2 transition duration-300 focus-within:border-[#8c7569]">
+                  <label className="text-xs uppercase font-semibold tracking-wide text-[#8c7569] transition duration-300">
+                    Event Type:
+                  </label>
+                  <select
+                    name="event_type"
+                    value={formData.event_type}
+                    onChange={handleChange}
+                    className="outline-none border-0 pt-1 text-xs placeholder-gray-300"
+                  >
+                    <option value="">Select</option>
+                    <option value="virtual">Virtual</option>
+                    <option value="offline">Offline</option>
+                  </select>
+                  {errors.event_type && (
+                    <div className="text-red-500">{errors.event_type}</div>
+                  )}
+                </div>
+                <div className="flex flex-col p-2 border border-[#ddd] rounded-[4px] mb-2 transition duration-300 focus-within:border-[#8c7569]">
+                  <label className="text-xs uppercase font-semibold tracking-wide text-[#8c7569] transition duration-300">
+                    Event Category:
+                  </label>
+                  <select
+                    name="event_category"
+                    value={formData.event_category}
+                    onChange={handleChange}
+                    className="outline-none border-0 pt-1 text-xs placeholder-gray-300"
+                  >
+                    <option value="">Select</option>
+                    <option value="Celebration">Celebration</option>
+                    <option value="EDM Party">EDM Party</option>
+                    <option value="Shows">Shows</option>
+                    <option value="Sports">Sports</option>
+                  </select>
+                  {errors.event_category && (
+                    <div className="text-red-500">{errors.event_category}</div>
+                  )}
+                </div>
+                <div className="flex flex-col p-2 border border-[#ddd] rounded-[4px] mb-2 transition duration-300 focus-within:border-[#8c7569]">
+                  <label className="text-xs uppercase font-semibold tracking-wide text-[#8c7569] transition duration-300">
+                    Event Description:
+                  </label>
+                  <input
+                    type="text"
+                    name="event_description"
+                    value={formData.event_description}
+                    placeholder="Enter event description"
+                    onChange={handleChange}
+                    className="outline-none border-0 pt-1 text-xs placeholder-gray-300"
+                  />
+                  {errors.event_description && (
+                    <div className="text-red-500">
+                      {errors.event_description}
+                    </div>
+                  )}
+                </div>
+                <div className="flex flex-col p-2 border border-[#ddd] rounded-[4px] mb-2 transition duration-300 focus-within:border-[#8c7569]">
+                  <label className="text-xs uppercase font-semibold tracking-wide text-[#8c7569] transition duration-300">
+                    Date of Event:
+                  </label>
+                  <input
+                    type="date"
+                    name="eventDate"
+                    value={formData.eventDate}
+                    onChange={handleChange}
+                    className="outline-none border-0 pt-1 text-xs placeholder-gray-300"
+                  />
+                  {errors.eventDate && (
+                    <div className="text-red-500">{errors.eventDate}</div>
+                  )}
+                </div>
+                <button
+                  className="mt-2 px-4 py-1 outline-none uppercase border-0 text-white rounded-[4px] bg-[#f2874d] transition duration-300 cursor-pointer font-nunito hover:bg-[#c04803]"
+                  type="submit"
+                >
+                  Organise
+                </button>
+              </form>
+              <div className="p-2">
+                <input type="checkbox" id="accept-terms-checkbox" />
+                <label htmlFor="accept-terms-checkbox" className="text-xs">
+                  I accept the{" "}
+                  <span className="text-red-500">Terms of Use</span> &{" "}
+                  <span className="text-red-500">Privacy Policy</span>
+                </label>
               </div>
-              {/* Other form inputs go here */}
-              <button className="input-button mt-2 py-0.5 px-3 outline-none text-xs text-white rounded bg-orange-500 transition duration-300 cursor-pointer">
-                Organise
-              </button>
-            </form>
-            <div className="accept-terms">
-              <input type="checkbox" id="accept-terms-checkbox" />
-              <label
-                htmlFor="accept-terms-checkbox"
-                className="text-gray-800 cursor-pointer"
-              >
-                I accept the{" "}
-                <span className="highlight text-red-600">Terms of Use</span> &{" "}
-                <span className="highlight text-red-600">Privacy Policy</span>
-              </label>
             </div>
-          </div>
-          <div className="modal-right hidden md:flex flex-grow">
-            {/* <img
-              src="https://plus.unsplash.com/premium_photo-1661286678499-211423a9ff5e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTQxfHxkYW5jZSUyMGZlc3RpdmFsfGVufDB8fDB8fHww"
-              alt=""
-              className="object-cover w-full h-full transform scale-100 transition duration-1200"
-            /> */}
+            <div className="flex-2 text-0 transition duration-300 overflow-hidden">
+              <img
+                src="https://plus.unsplash.com/premium_photo-1661286678499-211423a9ff5e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTQxfHxkYW5jZSUyMGZlc3RpdmFsfGVufDB8fDB8fHww"
+                alt=""
+                className="w-full h-full transform scale-100 object-cover transition duration-[1.2s]"
+              />
+            </div>
           </div>
         </div>
       </div>
